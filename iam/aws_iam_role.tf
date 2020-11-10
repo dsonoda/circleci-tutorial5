7ea9e83d5d1fda340fe5d5de60870f4df0e64ef8
daisuke sonoda <dksonoda@gmail.com>
2020-11-10 10:53:38 +0900

    modify

--- iam/aws_iam_role.tf
+++ iam/aws_iam_role.tf
@@ -5,7 +5,7 @@ resource "aws_iam_role" "ecs_instance_role" {
 }
 
 resource "aws_iam_role" "ecs_task_role" {
-  name               = "ecs-task-role"
+  name               = "ecs_task_role"
   path               = "/"
   assume_role_policy = file("aws_iam_role_policies/ecs_task_assume_role_policy.json")
 }

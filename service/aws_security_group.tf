712db9c37e8d6680cd02a34e46de56e0f6d0666d
daisuke sonoda <dksonoda@gmail.com>
2020-11-12 16:41:31 +0900

    modify

--- service/aws_security_group.tf
+++ service/aws_security_group.tf
@@ -2,46 +2,46 @@ resource "aws_security_group" "instance" {
   name        = "instance"
   description = "instance sg"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+}
 
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+resource "aws_security_group_rule" "instance_ingress" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = -1
+  source_security_group_id = aws_security_group.alb.id
+  security_group_id        = aws_security_group.instance.id
+}
 
-    security_groups = [
-      aws_security_group.alb.id
-    ]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "instance_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance.id
 }
 
 resource "aws_security_group" "alb" {
   name        = "sample-webapp-alb"
   description = "http and https"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+}
 
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "TCP"
+resource "aws_security_group_rule" "alb_ingress" {
+  security_group_id = aws_security_group.alb.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
+resource "aws_security_group_rule" "alb_egress" {
+  security_group_id = aws_security_group.alb.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  cidr_blocks       = ["0.0.0.0/0"]
 }
